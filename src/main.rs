14825f21f35649e54b720bc2f54121b494982d2a
MarkoBorkovic <markoborkovic@outlook.com>
2022-06-22 00:42:20 +0200

    Added command line support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod classifier;
 mod opcodes;
 
-use std::{fs::{File, self}, io::Read};
+use std::{fs::{File, self}, io::Read, env};
 
 use classifier::classify_intruction;
 
 fn main() {
-    let target: String = r"E:\gameboy\gameboy-roms\bootrom.gb".to_owned();
+    let args: Vec<String> = env::args().collect();
+    
+    let target: String = 
+        if args.len() > 1 
+            {args[1].clone()} 
+        else 
+            {r"E:\gameboy\gameboy-roms\bootrom.gb".to_owned()};
 
     let mut buffer: Vec<u8> = vec![];
 
@@ -20,12 +26,12 @@ fn main() {
     let mut current_byte = 0usize;
     while current_byte < buffer.len() {
         if let Some((instruction, consumed_bytes)) = classify_intruction(&mut buffer, &current_byte) {
-            let mut hex = "".to_owned();
-            for i in current_byte..=current_byte+consumed_bytes {
-                hex.push_str(&format!("{:02X} ", buffer[i]));
-            }
+            // let mut hex = "".to_owned();
+            // for i in current_byte..=current_byte+consumed_bytes {
+            //     hex.push_str(&format!("{:02X} ", buffer[i]));
+            // }
 
-            println!("{} <=> {}", instruction, hex);
+            println!("{}", instruction);
             current_byte += consumed_bytes;
         }
         current_byte += 1;
